ddbbc5cd3ecc73a5dd5c61cbbb907e73dc7a43d4
Mary Wong <pair+mwong@modcloth.com>
2013-01-23 10:37:08 -0800

    made test example names more clear

--- spec/piglatin_spec.rb
+++ spec/piglatin_spec.rb
@@ -2,27 +2,27 @@ require "piglatin"
 
 describe PigLatin do
   describe '#translate' do
-    it 'translates "Hello" correctly' do
+    it 'translates simple single word correctly' do
       subject.class.translate("Hello").should == "ello-Hay"
     end
 
-    it 'translates "Hello world" correctly' do
+    it 'translates multiple words correctly' do
       subject.class.translate("Hello world").should == "ello-Hay orld-way"
     end
 
-    it 'translates "  Hello      world  " correctly' do
+    it 'translates multiple words with extra spaces correctly' do
       subject.class.translate("  Hello      world  ").should == "ello-Hay orld-way"
     end
 
-    it 'translates "Hello World" correctly' do
+    it 'translates multiple words start case correctly' do
       subject.class.translate("Hello World").should == "ello-Hay orld-Way"
     end
 
-    it 'translates "skate" correctly' do
+    it 'translates "word starting multiple consonants" correctly' do
       subject.class.translate("skate").should == "ate-skay"
     end
 
-    it 'translate "awesome" correctly' do
+    it 'translate "word staring with vowel" correctly' do
       subject.class.translate("awesome").should == "awesome-way"
     end
 
@@ -34,38 +34,38 @@ describe PigLatin do
       subject.class.translate("way").should == "ay-way"
     end
 
-    it 'translate "tsk" correctly' do
+    it 'translate "voweless word" correctly' do
       subject.class.translate("tsk").should == "-tskay"
     end
   end
 
   describe '#revert' do
-    it 'reverts "ello-Hay" correctly' do
+    it 'reverts simple word correctly' do
       subject.class.revert("ello-Hay").should == "Hello"
     end
 
-    it 'reverts "ello-Hay orld-Way" correctly' do
+    it 'reverts multiple words correctly' do
       subject.class.revert("ello-Hay orld-Way").should == "Hello World"
     end
 
-    it 'reverts "ate-skay" correctly' do
+    it 'reverts word staring with multiple consonants correctly' do
       subject.class.revert("ate-skay").should == "skate"
     end
 
-    it 'reverts "-tskay" correctly' do
+    it 'reverts voweless word correctly' do
       subject.class.revert("-tskay").should == "tsk"
     end
 
-    describe 'cannot resolve originals' do
-      it 'reverts "ello-Hay orld-way" results in getting exception' do
+    describe 'cannot resolve original' do
+      it 'raises exception when cannot tell if original is "Hello world" or "Hello orld"' do
         lambda { subject.class.revert("ello-Hay orld-way") }.should raise_error
       end
 
-      it 'reverts "awesome-way" results in getting exception' do
+      it 'raises exception when cannot tell if original is "awesome" or "wawesome"' do
         lambda { subject.class.revert("awesome-way") }.should raise_error
       end
 
-      it 'reverts "ay-way" results in getting exception' do
+      it 'raises exception when cannot tell if original is "ay" or "way"' do
         lambda { subject.class.revert("ay-way") }.should raise_error
       end
     end
